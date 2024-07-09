552a0392ffa7c8205272e9152b4ad87d5ef1e3d5
JovanaRadisavljevic <radisavljevic435@gamil.com>
2024-07-09 19:20:41 +0200

    prvi deo stranice home

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Button } from './Button'
+import './css/HeroSection.css'
 
-const heroSection = () => {
+const HeroSection = () => {
   return (
-    <div>
-      
+    <div className='hero-container'>
+       <video src='/videos/video-2.mp4' autoPlay loop muted />
+        <h1>AVANTURA TE CEKA</h1>
+        <p>Šta čekaš?</p>
+        <div className='hero-btns'>
+            <Button className='btns' buttonStyle='btn--otline'
+            buttonSize='btn--large' >
+                PRIJAVI SE ODMAH!
+            </Button>
+            <Button className='btns' buttonStyle='btn--primary'
+            buttonSize='btn--large' >
+                POGLEDAJ TRAILER <i className='far fa-play-circle' />
+            </Button>
+        </div>
     </div>
   )
 }
 
-export default heroSection
+export default HeroSection
